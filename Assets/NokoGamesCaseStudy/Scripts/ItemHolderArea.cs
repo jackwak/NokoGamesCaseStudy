5c6704940a7260f59982b45e6cb08bc7536da880
jackwak <mehmetyanik53@hotmail.com>
2025-11-07 03:22:41 +0300

    Update ItemHolderArea.cs

--- Assets/NokoGamesCaseStudy/Scripts/ItemHolderArea.cs
+++ Assets/NokoGamesCaseStudy/Scripts/ItemHolderArea.cs
@@ -5,50 +5,42 @@ using UnityEngine;
 public class ItemHolderArea : MonoBehaviour
 {
     [Header(" References ")]
-    [SerializeField] private Renderer _holderArea;
-    [SerializeField] private GameObject _areaHolderObject;
-
+    [SerializeField] private Renderer _areaRenderer;
 
     [Header(" Settings ")]
     [SerializeField] private int _heightCount;
     [SerializeField] private int _xCount;
     [SerializeField] private int _zCount;
-
     [SerializeField] private float _xSpacing;
     [SerializeField] private float _zSpacing;
 
-    [Header(" Datas ")]
-    private List<ItemData> _itemDatas;
 
-    public class ItemData
-    {
-        public Vector3 ItemPosition;
-        public bool PositionOccupied;
-        public GameObject ItemObject;
-    }
+    private List<Vector3> _itemPositions;
+    private Stack<GameObject> _itemStack;
 
     private void Awake()
     {
-        _itemDatas = new List<ItemData>();
+        _itemPositions = new List<Vector3>();
+        _itemStack = new Stack<GameObject>();
     }
 
-    void Start()
+    private void Start()
     {
-        InitializeItemPositions(_areaHolderObject.transform.GetChild(0).GetComponent<Renderer>().bounds.size);
+        InitializeItemPositions(_areaRenderer.transform.GetChild(0).GetComponent<Renderer>().bounds.size);
     }
 
     public void InitializeItemPositions(Vector3 itemSize)
     {
-        _itemDatas.Clear();
+        _itemPositions.Clear();
+        _itemStack.Clear();
 
         float totalWidth = (_xCount - 1) * (itemSize.x + _xSpacing) + itemSize.x;
         float totalDepth = (_zCount - 1) * (itemSize.z + _zSpacing) + itemSize.z;
-
-        Vector3 holderCenter = _holderArea.bounds.center;
+        Vector3 holderCenter = _areaRenderer.bounds.center;
 
         Vector3 startPos = new Vector3(
             holderCenter.x - (totalWidth / 2f) + (itemSize.x / 2f),
-            _holderArea.bounds.min.y,
+            _areaRenderer.bounds.min.y,
             holderCenter.z - (totalDepth / 2f) + (itemSize.z / 2f)
         );
 
@@ -64,141 +56,35 @@ public class ItemHolderArea : MonoBehaviour
                         startPos.z + (z * (itemSize.z + _zSpacing))
                     );
 
-                    ItemData data = new ItemData
-                    {
-                        ItemPosition = position,
-                        PositionOccupied = false,
-                        ItemObject = null
-                    };
-
-                    _itemDatas.Add(data);
+                    _itemPositions.Add(position);
                 }
             }
         }
     }
 
-    public Vector3? GetAvailablePosition()
+    public void AddItem(GameObject item)
     {
-        for (int i = 0; i < _itemDatas.Count; i++)
-        {
-            if (!_itemDatas[i].PositionOccupied)
-            {
-                _itemDatas[i].PositionOccupied = true;
-                return _itemDatas[i].ItemPosition;
-            }
-        }
-
-        return null;
+        _itemStack.Push(item);
     }
 
-    public Vector3? AddItem(GameObject item)
+    public GameObject RemoveItem()
     {
-        for (int i = 0; i < _itemDatas.Count; i++)
+        if (_itemStack.Count == 0)
         {
-            if (!_itemDatas[i].PositionOccupied)
-            {
-                _itemDatas[i].PositionOccupied = true;
-                _itemDatas[i].ItemObject = item;
-                return _itemDatas[i].ItemPosition;
-            }
+            return null;
         }
 
-        return null;
+        return _itemStack.Pop();
     }
 
-    public GameObject RemoveLastItem()
+    public Vector3? GetAvaiblePosition()
     {
-        for (int i = _itemDatas.Count - 1; i >= 0; i--)
+        if (_itemStack.Count >= _itemPositions.Count)
         {
-            if (_itemDatas[i].PositionOccupied && _itemDatas[i].ItemObject != null)
-            {
-                GameObject item = _itemDatas[i].ItemObject;
-                
-                _itemDatas[i].PositionOccupied = false;
-                _itemDatas[i].ItemObject = null;
-
-                return item;
-            }
+            return null;
         }
 
-        return null;
+        Vector3 nextPos = _itemPositions[_itemStack.Count];
+        return nextPos;
     }
-
-    public void ReleasePosition(Vector3 position)
-    {
-        for (int i = 0; i < _itemDatas.Count; i++)
-        {
-            if (Vector3.Distance(_itemDatas[i].ItemPosition, position) < 0.01f)
-            {
-                _itemDatas[i].PositionOccupied = false;
-                _itemDatas[i].ItemObject = null;
-                return;
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (_itemDatas == null || _itemDatas.Count == 0) return;
-
-        foreach (var data in _itemDatas)
-        {
-            Gizmos.color = data.PositionOccupied ? Color.red : Color.green;
-            Gizmos.DrawWireSphere(data.ItemPosition, 0.1f);
-        }
-    }
-
-
-#if UNITY_EDITOR
-    [ContextMenu("Test - Spawn Objects")]
-    private void TestSpawn()
-    {
-        if (_areaHolderObject != null)
-        {
-            _itemDatas = new List<ItemData>();
-
-            Renderer testRenderer = _areaHolderObject.transform.GetChild(0).GetComponent<Renderer>();
-            if (testRenderer != null)
-            {
-                Vector3 itemSize = testRenderer.bounds.size;
-                InitializeItemPositions(itemSize);
-                SpawnTestObjects();
-            }
-        }
-    }
-
-    private void SpawnTestObjects()
-    {
-        foreach (var data in _itemDatas)
-        {
-            GameObject obj = Instantiate(_areaHolderObject, data.ItemPosition, Quaternion.identity, transform);
-            AddItem(obj);
-        }
-    }
-
-    [ContextMenu("Test - Clear Objects")]
-    private void TestClear()
-    {
-        for (int i = transform.childCount - 1; i >= 1; i--)
-        {
-            DestroyImmediate(transform.GetChild(i).gameObject);
-        }
-        _itemDatas.Clear();
-    }
-
-    [ContextMenu("Test - Remove Last Item")]
-    private void TestRemoveLastItem()
-    {
-        GameObject lastItem = RemoveLastItem();
-        if (lastItem != null)
-        {
-            Debug.Log($"Çıkarılan item: {lastItem.name}");
-            DestroyImmediate(lastItem);
-        }
-        else
-        {
-            Debug.Log("Çıkarılacak item yok!");
-        }
-    }
-#endif
 }
