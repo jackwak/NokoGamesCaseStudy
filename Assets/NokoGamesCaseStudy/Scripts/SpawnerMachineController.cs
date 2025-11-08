b2906fb1b7b26384948dbfec2419cf891d56b393
jackwak <mehmetyanik53@hotmail.com>
2025-11-08 13:57:21 +0300

    Inhereted Spawner and Transformer machine controllers from base

--- Assets/NokoGamesCaseStudy/Scripts/SpawnerMachineController.cs
+++ Assets/NokoGamesCaseStudy/Scripts/SpawnerMachineController.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class SpawnerMachineController : MonoBehaviour
+public class SpawnerMachineController : BaseMachineController
 {
-    // Start is called before the first frame update
+    [Header(" Settings ")]
+    [SerializeField] private float _spawnInterval;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        InvokeRepeating(nameof(TransformItem), _spawnInterval, _spawnInterval);
     }
 }
--- Assets/NokoGamesCaseStudy/Scripts/TransformerMachineController.cs
+++ Assets/NokoGamesCaseStudy/Scripts/TransformerMachineController.cs
@@ -3,24 +3,21 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class TransformerMachineController : MonoBehaviour
+public class TransformerMachineController : BaseMachineController
 {
     [Header(" References ")]
     [SerializeField] private ItemHolderArea _dropArea;
-    [SerializeField] private ItemHolderArea _collectArea;
-    [SerializeField] private GameObject _transformedItem;
     [SerializeField] private Transform _entryTransform;
-    [SerializeField] private Transform _exitTransform;
 
-    [Header(" Settings ")]
-    [SerializeField] private float _itemCollectInterval = .15f;
-    [SerializeField] private float _startCollectDelay = .2f;
-    [SerializeField] private float _itemArriveTime = .1f;
+    [Header(" Datas ")]
+    private Vector3 _entryPosition;
 
-    //Properties
-    private Vector3 EntryPosition => _entryTransform.position;
-    private Vector3 ExitPosition => _exitTransform.position;
-    private Transform CollectAreaItemHolder => _collectArea.transform.GetChild(1);
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _entryPosition = _entryTransform.position;
+    }
 
     public void StartTransform()
     {
@@ -29,26 +26,15 @@ public class TransformerMachineController : MonoBehaviour
 
     private IEnumerator StartTransformCoroutine()
     {
-        yield return _startCollectDelay;
+        yield return _machineSettings.StartCollectDelay;
 
         while (true)
         {
             GameObject lastItem = _dropArea.RemoveItem();
             if (lastItem == null) yield break;
-            lastItem.transform.DOMove(EntryPosition, _itemArriveTime).OnComplete(() => TransformItem());
+            lastItem.transform.DOMove(_entryPosition, _machineSettings.ItemArriveTime).OnComplete(() => TransformItem());
 
-            yield return new WaitForSeconds(_itemCollectInterval);
+            yield return new WaitForSeconds(_machineSettings.ItemCollectInterval);
         }
-    }
-
-    private void TransformItem()
-    {
-        Vector3? itemDropPosition = _collectArea.GetAvaiblePosition();
-        if (itemDropPosition == null) return;
-
-        GameObject transformedItem = Instantiate(_transformedItem, CollectAreaItemHolder);
-
-        transformedItem.transform.position = ExitPosition;
-        transformedItem.transform.DOMove((Vector3)itemDropPosition, _itemArriveTime).OnComplete(() => _collectArea.AddItem(transformedItem));
     }
 }
