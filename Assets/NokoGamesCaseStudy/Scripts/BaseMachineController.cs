8bd1ceb34b7cb95c9bd3074272097fdbc58620b5
jackwak <mehmetyanik53@hotmail.com>
2025-11-08 13:55:01 +0300

    Added BaseMachineController.cs

---
+++ Assets/NokoGamesCaseStudy/Scripts/BaseMachineController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BaseMachineController : MonoBehaviour
+{
+    [Header(" Referenes ")]
+    [SerializeField] protected GameObject _transformedItem;
+    [SerializeField] protected ItemHolderArea _collectArea;
+    [SerializeField] protected Transform _exitTransform;
+    [SerializeField] protected Animator _animator;
+
+    [Header(" Settings ")]
+    [SerializeField] protected MachineSettings _machineSettings;
+
+    [Header(" Datas ")]
+    protected Vector3 _exitPosition;
+    protected Transform _collectAreaItemHolder;
+    private bool _isWorking = false;
+    private int _isWorkingHash;
+
+    protected virtual void Awake()
+    {
+        _exitPosition = _exitTransform.position;
+        _collectAreaItemHolder = _collectArea.transform.GetChild(1);
+        _isWorkingHash = Animator.StringToHash("IsWorking");
+    }
+
+    protected Vector3? HasAvaiblePosition()
+    {
+        return _collectArea.GetAvaiblePosition();
+    }
+
+    protected void TransformItem()
+    {
+        Vector3? itemDropPosition = HasAvaiblePosition();
+
+        if (itemDropPosition == null)
+        {
+            SetMachineState(false);
+            return;
+        }
+
+        SetMachineState(true);
+
+        GameObject transformedItem = Instantiate(_transformedItem, _collectAreaItemHolder);
+        transformedItem.transform.position = _exitPosition;
+        transformedItem.transform.DOJump((Vector3)itemDropPosition, 1f, 1, _machineSettings.ItemArriveTime)
+            .OnComplete(() => _collectArea.AddItem(transformedItem));
+    }
+
+    protected void SetMachineState(bool isWorking)
+    {
+        if (_isWorking == isWorking) return;
+
+        _isWorking = isWorking;
+        _animator?.SetBool(_isWorkingHash, _isWorking);
+    }
+    
+    protected void OnDisable()
+    {
+        DOTween.Kill(transform);
+    }
+}
